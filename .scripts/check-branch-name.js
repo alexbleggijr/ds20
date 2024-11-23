53b233c250c4fe48c4b03b5a4cd9863a0b8ebc68
Alex Bleggi <149841160+alexbleggijr@users.noreply.github.com>
2024-11-23 19:51:14 -0300

    [angular-material][fix] teste de titulo certo (#5)

--- .scripts/check-branch-name.js
+++ .scripts/check-branch-name.js
@@ -12,35 +12,36 @@ const branchName = execSync('git rev-parse --abbrev-ref HEAD')
 // check if the branch name matches the pattern
 if (!branchPattern.test(branchName)) {
   console.error(
-    `Invalid branch name: ${branchName}. Branch names must match the pattern: {package}/{type}/task-to-be-done,`,
+    `Invalid branch name: ${branchName}. Branch names must match the pattern: {package}/{type}/task-to-be-done`,
   );
   console.log('');
   console.warn(
-    `{package}: 
-      - angular-material 
-      - icons
-      - interactions
-      - ionic-5
-      - ionic-6
-      - ionic-7
-      - ionic-8
-      - shared
-      - tokens
-      - utilities
-      - web-components`,
+    `{package}:
+  - root 
+  - angular-material 
+  - icons
+  - interactions
+  - ionic-5
+  - ionic-6
+  - ionic-7
+  - ionic-8
+  - shared
+  - tokens
+  - utilities
+  - web-components`,
   );
-  console.warn(`
-    {type}: 
-      - feat: a new feature
-      - fix: a bug fix
-      - docs: documentation only (readme, jsDoc, storybook)
-      - style: code formatting only (prettier, stylelint, ESLint)
-      - refactor: code refactoring
-      - test: unit or visual regression
-      - build: TODO
-      - ci: TODO
-      - chore: other changes that don't modify 'src' or 'test' files
-    `);
+  console.warn(
+    `{type}: 
+  - feat: a new feature
+  - fix: a bug fix
+  - docs: documentation only (readme, jsDoc, storybook)
+  - style: code formatting only (prettier, stylelint, ESLint)
+  - refactor: code refactoring
+  - test: unit or visual regression
+  - build: TODO
+  - ci: TODO
+  - chore: other changes that don't modify 'src' or 'test' files`,
+  );
   process.exit(1); // exit with error code to block commit/push
 }
 
