3bcade5feee85f3711722c894a6b04d0e89d10f7
alexandre.junior <alexandre.junior@terceirizados.medgrupo.com.br>
2024-11-26 16:35:28 -0300

    commit

--- .scripts/check-branch-name.js
+++ .scripts/check-branch-name.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process');
 
 // regular expression to enforce branch naming convention
 const branchPattern =
-  /^(root|angular-material|icons|interactions|ionic-5|ionic-6|ionic-7|ionic-8|shared|tokens|utilities|web-components)+\/(feat|fix|docs|style|refactor|test|build|ci|chore)\/[a-z0-9\-]+$/;
+  /^(feat|fix|docs|style|refactor|test|build|ci|chore)+\/(root|angular-material|icons|interactions|ionic-5|ionic-6|ionic-7|ionic-8|shared|tokens|utilities|web-components)\/[a-z0-9\-]+$/;
 
 // get the current branch name
 const branchName = execSync('git rev-parse --abbrev-ref HEAD')
@@ -12,9 +12,21 @@ const branchName = execSync('git rev-parse --abbrev-ref HEAD')
 // check if the branch name matches the pattern
 if (!branchPattern.test(branchName)) {
   console.error(
-    `Invalid branch name: ${branchName}. Branch names must match the pattern: {package}/{type}/task-to-be-done`,
+    `Invalid branch name: ${branchName}. Branch names must match the pattern: {type}/{package}/task-to-be-done`,
   );
   console.log('');
+  console.warn(
+    `{type}: 
+  - feat: a new feature
+  - fix: a bug fix
+  - docs: documentation only (readme, jsDoc, storybook)
+  - style: code formatting only (prettier, stylelint, ESLint)
+  - refactor: code refactoring
+  - test: unit or visual regression
+  - build: TODO
+  - ci: TODO
+  - chore: other changes that don't modify 'src' or 'test' files`,
+  );
   console.warn(
     `{package}:
   - root 
@@ -29,18 +41,6 @@ if (!branchPattern.test(branchName)) {
   - tokens
   - utilities
   - web-components`,
-  );
-  console.warn(
-    `{type}: 
-  - feat: a new feature
-  - fix: a bug fix
-  - docs: documentation only (readme, jsDoc, storybook)
-  - style: code formatting only (prettier, stylelint, ESLint)
-  - refactor: code refactoring
-  - test: unit or visual regression
-  - build: TODO
-  - ci: TODO
-  - chore: other changes that don't modify 'src' or 'test' files`,
   );
   process.exit(1); // exit with error code to block commit/push
 }
